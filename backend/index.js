e9d197b94a50651f35600a42b9f1fce110330321
Aditya <adityarajgarkal@gmail.com>
2026-04-05 17:35:59 +0530

    something

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,14 @@ const mongoose = require("mongoose")
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//---------------mongoDB connection-----------------------
+
 mongoose.connect('mongodb://127.0.0.1:27017/interview_analyzer')
     .then(() => {console.log("MongoDB connected")})
     .catch(err => console.log(err))
+
+//---------------Routes-----------------------    
     
 app.use('/', userRouter);
+
 app.listen(PORT, ()=> {console.log(`Server started at port: ${PORT}`)});
